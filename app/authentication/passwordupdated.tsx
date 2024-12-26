a041e1804ad4f7ae08c3239f83f6269a137fb699
nguyenmaikhanh <nguyenmaikhanh713@gmail.com>
2024-12-26 13:46:48 +0700

    minor change

--- app/authentication/passwordupdated.tsx
+++ app/authentication/passwordupdated.tsx
@@ -1,18 +1,12 @@
 import React from "react";
-import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import RoundedButton from "@/component/RoundedButton";
 import { router } from "expo-router";
 
-interface PasswordUpdatedProps {
-  onBackToLogin: () => void;
-}
-
-export default function PasswordUpdated({
-  onBackToLogin,
-}: PasswordUpdatedProps) {
+export default function PasswordUpdated() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <View style={styles.formContainer}>
         <Image
           source={require("../../assets/images/SuccessfulIcon.png")}
@@ -41,45 +35,39 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    padding: 20,
-  },
-  partContainer: {
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: "#fff", // Đặt nền trắng nếu cần
   },
   formContainer: {
-    flex: 1, // Chiếm toàn bộ chiều cao của container
-    justifyContent: "center", // Căn giữa theo chiều dọc
-    alignItems: "center", // Căn giữa theo chiều ngang
-    gap: 10, // Khoảng cách giữa các thành phần
-    width: "100%", // Sử dụng toàn bộ chiều rộng
-    paddingBottom: 0, // Xóa padding không cần thiết
-    paddingHorizontal: 447,
-    paddingVertical: 150,
+    width: 514, // Chiều rộng cố định
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    gap: 30, // Khoảng cách giữa các phần tử
+    paddingVertical: 50, // Căn lề dọc
   },
-
+  icon: {
+    width: 174,
+    height: 174,
+    aspectRatio: 1, // Tỉ lệ 1:1 cho hình vuông
+    marginBottom: 20,
+  },
   header: {
     fontFamily: "Roboto_700Bold",
     fontSize: 48,
     fontWeight: "bold",
     color: "black",
-    marginBottom: 20,
     textAlign: "center",
+    width: "100%", // Chiếm toàn bộ chiều rộng container
   },
   notice: {
     fontFamily: "Roboto_400Regular",
     fontSize: 20,
     color: "gray",
     textAlign: "center",
-    marginBottom: 0,
+    width: "100%", // Chiếm toàn bộ chiều rộng container
   },
   button: {
-    width: "100%",
+    width: "100%", // Chiếm toàn bộ chiều rộng container
     marginTop: 30,
-  },
-  icon: {
-    width: 100,
-    height: 100,
-    marginBottom: 20,
   },
 });
