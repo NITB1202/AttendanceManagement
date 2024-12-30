3fd25899ec8ad764dfea75a43c6fd055887c841f
KhanhPham <161315638+LSFKazuha@users.noreply.github.com>
2024-12-30 12:25:35 +0700

    complete 1/2 pop up

--- app/section_manager/class_manager.tsx
+++ app/section_manager/class_manager.tsx
@@ -12,6 +12,7 @@ import {
 import Layout from "../../component/Layout"; // Import Layout component
 import TableComponent from "../../component/Table"; // Import TableComponent của bạn
 import SearchBar from "@/component/SearchBar";
+import RoundedButton from "@/component/RoundedButton";
 
 const handleSearch = (query: string) => {
   console.log("Từ khóa tìm kiếm:", query);
@@ -95,6 +96,10 @@ const ClassManager = () => {
         >
           <View style={styles.modalContainer}>
             <View style={styles.modalView}>
+              <TouchableOpacity style={styles.closeButton} onPress={() => setModalVisible(false)}>
+                <Text style={styles.closeButtonText}>✕</Text>
+              </TouchableOpacity>
+
               <Text style={styles.modalText}>Create New Class</Text>
 
               <View style={styles.contentContainer}>
@@ -116,10 +121,10 @@ const ClassManager = () => {
                 <View style={styles.contentBox2}>
                   <View style={styles.column}>
                     <Text style={styles.label}>Start Date</Text>
-                    <TextInput style={styles.inputTime} placeholder="Input 1" />
+                    <TextInput style={styles.input} placeholder="" />
                     <Text style={styles.label}>Start Time</Text>
                     <View style={styles.row}>
-                      <TextInput style={styles.inputTime} placeholder="Input 1" />
+                      <TextInput style={styles.inputTime} placeholder="" />
                       <select style={styles.dropdownTime}>
                         <option value="class1">AM</option>
                         <option value="class2">PM</option>
@@ -128,10 +133,10 @@ const ClassManager = () => {
                   </View>
                   <View style={styles.column}>
                     <Text style={styles.label}>End Date</Text>
-                    <TextInput style={styles.input} placeholder="Input 2" />
+                    <TextInput style={styles.input} placeholder="" />
                     <Text style={styles.label}>End Time</Text>
                     <View style={styles.row}>
-                      <TextInput style={styles.inputTime} placeholder="Input 1" />
+                      <TextInput style={styles.inputTime} placeholder="" />
                       <select style={styles.dropdownTime}>
                         <option value="class1">AM</option>
                         <option value="class2">PM</option>
@@ -143,12 +148,12 @@ const ClassManager = () => {
                   <View style={styles.column2}>
                     <Text style={styles.label}>Teacher Name</Text>
                     <select style={styles.dropdownTeacher}>
-                      <option value="class1">AM</option>
-                      <option value="class2">PM</option>
+                      <option value="class1">Brian Anna</option>
+                      <option value="class2">Paul Gaspart</option>
                     </select>
                     <Text style={styles.label}>Student List</Text>
                     <View style={styles.textBoxList}>
-                      <Text style={styles.text}>SV3340112</Text>
+                      <Text style={styles.text}></Text>
                     </View>
                   </View>
                   <View style={styles.column3}>
@@ -156,13 +161,24 @@ const ClassManager = () => {
                     <View style={styles.textBoxCode}>
                       <Text style={styles.text}>SV3340112</Text>
                     </View>
+                    <RoundedButton
+                      title="Upload Excel File"
+                      onPress={() => { }}
+                      style={styles.roundedButton}
+                      textStyle={styles.roundedButtonText}
+                    />
                   </View>
                 </View>
               </View>
   
               <View style={styles.buttonContainer}>
-                <Button title="Save" onPress={handleSave} />
-                <Button title="Cancel" onPress={() => setModalVisible(false)} />
+                <RoundedButton
+                  title="CONFIRM"
+                  onPress={handleSave}
+                  style={styles.roundedButton}
+                  textStyle={styles.roundedButtonSave}
+                />
+                
               </View>
             </View>
           </View>
@@ -237,7 +253,7 @@ const styles = StyleSheet.create({
   },
   modalView: {
     width: 523,
-    height: 745,
+    height: 670,
     backgroundColor: 'white',
     borderRadius: 10,
     padding: 20,
@@ -374,5 +390,30 @@ const styles = StyleSheet.create({
     fontSize: 16,
     flexDirection: 'row',
     flex: 1,
+  },
+  roundedButtonText: {
+    fontSize: 14, 
+    fontWeight: 'bold', 
+  },
+  roundedButtonSave: {
+    fontSize: 20, 
+    fontWeight: 'bold', 
+  },
+  roundedButton: {
+    marginTop: 25, 
+    width: '100%', 
+    height: 40, 
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    padding: 10,
+    borderRadius: 15,
+  },
+  closeButtonText: {
+    color: 'black',
+    fontSize: 24,
+    
   },
 });
